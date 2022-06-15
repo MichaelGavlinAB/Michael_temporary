4de13523ec7b75dd33436e88cea7b94d7f3e87a2
MichaelGavlinAB <105149503+MichaelGavlinAB@users.noreply.github.com>
2022-06-15 16:25:11 +0300

    Add files via upload

--- plotly_hello_worl.py
+++ plotly_hello_worl.py
@@ -23,11 +23,11 @@ pd.options.plotting.backend = "plotly"
 Michael function - Read CSV file and extract data
 """
 dff = pd.read_csv('/home/michael/Desktop/Recordings/EBA/618e558a8fa38d33e9d69d42/2020.10.09_at_15.37.30_camera-mi_5022_extract_1602259269281220us_to_1602259311323110us_id0.csv')
-dff_relevant = dff [['MTS.Package.TimeStamp','MFC5xx Device.VDY.VehDyn.Longitudinal.Velocity','MFC5xx Device.VDY.VehDyn.Longitudinal.varAccel','MFC5xx Device.VDY.VehDyn.Lateral.YawRate.YawRate']]
-print("relevant data is: \n" , dff_relevant)
+# dff_relevant = dff [['MTS.Package.TimeStamp','MFC5xx Device.VDY.VehDyn.Longitudinal.Velocity','MFC5xx Device.VDY.VehDyn.Longitudinal.varAccel','MFC5xx Device.VDY.VehDyn.Lateral.YawRate.YawRate']]
+print("relevant data is: \n" , dff)
 
 header = []
-for ii in dff_relevant:
+for ii in dff:
     header.append(ii)
 print("Header is: \n",header)
 
@@ -41,7 +41,7 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.VAPOR])
 mytitle = dcc.Markdown(children='# Mapi + CSV reader display tool')
 mytitle2 = dcc.Markdown(children='# System engineering')
 mygraph = dcc.Graph(figure={})
-dropdown = dcc.Dropdown(options=[header[0],header[1],header[2],header[3]],
+dropdown = dcc.Dropdown(options=header,
                         clearable=False)
 
 # Customize your own Layout
@@ -53,30 +53,10 @@ app.layout = dbc.Container([mytitle , mygraph, dropdown , mytitle2])
     Input(dropdown, component_property='value')
 )
 
-# def update_graph(user_input):  # function arguments come from the component property of the Input
-#     if user_input == 'Bar Plot':
-#         fig = px.bar(data_frame=dff_pandas, x="a", y="b")
-#
-#     elif user_input == 'Scatter Plot':
-#         fig = px.scatter(data_frame=dff_pandas, x="a", y="b")
-#     return fig  # returned objects are assigned to the component property of the Output
-
 def update_graph(user_input):  # function arguments come from the component property of the Input
+    fig = px.scatter(data_frame=dff, x=header[0], y=user_input)
 
-    if user_input == header[0]:
-        print(header[0])
-        fig = px.scatter(data_frame=dff_relevant, x=header[0], y=header[0])
-    elif user_input == header[1]:
-        print(header[1])
-        fig = px.scatter(data_frame=dff_relevant, x=header[0], y=header[1])
-    elif user_input == header[2]:
-        print(header[2])
-        fig = px.scatter(data_frame=dff_relevant, x=header[0], y=header[2])
-    elif user_input == header[3]:
-        print(header[3])
-        fig = px.scatter(data_frame=dff_relevant, x=header[0], y=header[3])
     return fig  # returned objects are assigned to the component property of the Output
-
 
 # Run app
 if __name__=='__main__':
