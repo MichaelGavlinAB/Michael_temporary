c2dd129e168da6d664030a542d76ccfeba188998
MichaelGavlinAB <105149503+MichaelGavlinAB@users.noreply.github.com>
2022-06-16 16:21:08 +0300

    Add files via upload

--- dropin_temp.py
+++ dropin_temp.py
@@ -1,7 +1,6 @@
 import base64
 import datetime
 import io
-
 import dash
 from dash.dependencies import Input, Output, State
 from dash import dcc, html, dash_table
@@ -15,11 +14,7 @@ temporary read header
 
 dff = pd.read_csv('/home/michael/Desktop/Recordings/EBA/618e558a8fa38d33e9d69d42/2020.10.09_at_15.37.30_camera-mi_5022_extract_1602259269281220us_to_1602259311323110us_id0.csv')
 print("relevant data is: \n" , dff)
-header = []
-for ii in dff:
-    header.append(ii)
-print("Header is: \n",header)
-#######
+print("Header2222 is \n" , dff.columns.tolist())
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -27,6 +22,8 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
     dcc.Markdown(children='# Mapi + CSV reader display tool'),
+    dcc.Graph(id='mygraph' , figure={}),
+    dcc.Dropdown(id = 'dropdown' , options=dff.columns.tolist() , clearable=False),
     dcc.Upload(
         id='upload-data',
         children=html.Div([
@@ -46,15 +43,13 @@ app.layout = html.Div([
         # Allow multiple files to be uploaded
         multiple=True
     ),
-    html.Div(id='output-data-upload'),
-    dcc.Graph(id='mygraph' , figure={}),
-    dcc.Dropdown(id = 'dropdown' , options=header , clearable=False)
 
+    html.Div(id='output-data-upload')
 ])
 
 def parse_contents(contents, filename, date):
     content_type, content_string = contents.split(',')
-
+    global df
     decoded = base64.b64decode(content_string)
     try:
         if 'csv' in filename:
@@ -86,7 +81,8 @@ def parse_contents(contents, filename, date):
         html.Pre(contents[0:200] + '...', style={
             'whiteSpace': 'pre-wrap',
             'wordBreak': 'break-all'
-        })
+
+        } )
     ])
 
 @app.callback(Output('output-data-upload', 'children'),
@@ -94,13 +90,15 @@ def parse_contents(contents, filename, date):
               State('upload-data', 'filename'),
               State('upload-data', 'last_modified'))
 
-
-def update_output(list_of_contents, list_of_names, list_of_dates):
+def children(list_of_contents, list_of_names, list_of_dates):
     if list_of_contents is not None:
         children = [
             parse_contents(c, n, d) for c, n, d in
             zip(list_of_contents, list_of_names, list_of_dates)]
+        print("imported type is:" , type(df))  # this will show data type as a pandas dataframe
+        print("The impoted data is: \n" , df)
         return children
+
 
 @app.callback(
     Output(component_id='mygraph', component_property='figure'),
@@ -109,11 +107,8 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
 )
 
 def update_graph(user_input):  # function arguments come from the component property of the Input
-    fig = px.scatter(data_frame=dff, x=header[0], y=user_input)
+    fig = px.scatter(data_frame=dff, x=dff.columns.tolist()[0], y=user_input)
     return fig  # returned objects are assigned to the component property of the Output
-
-
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
