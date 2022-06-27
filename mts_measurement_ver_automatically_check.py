27dba84ce34c449214aefdbc45c79822461f637b
MichaelGavlinAB <105149503+MichaelGavlinAB@users.noreply.github.com>
2022-06-27 16:40:40 +0100

    Add files via upload

--- mts_measurement_ver_automatically_check.py
+++ mts_measurement_ver_automatically_check.py
@@ -4,20 +4,22 @@ from os import listdir
 from os.path import isfile, join
 import shutil
 from cartica_utilities.utilities import os_utilities
-# import subprocess
 
 """
-Agenda - 
-    to check a recc file for errors using diffrent mts_measurement versions that
-    stored in folder as 7z format, automatically. 
+script purpose:
+    check a recc file for errors using different mts_measurement versions that
+    stored in folder as 7z format, automatically one by one.
+    all results are stored at "mts_measurement_7z_folder" path as folders.
+
 notes:
-    * Folder "mypath" shall contain only 7z files 
-    * first delete mts_measurement from origin because delete function does now work onb windows 
+    * Folder "mts_measurement_7z_folder" shall contain only 7z files.
+    * first delete mts_measurement from origin path.
     * use extractor_vers2.cfg configuration
-what it does:
-    1. extract new mts_measurement from 7z file
+what the script do:
+    1. extract new mts_measurement from 7z file. you can put as many 7z files as you wannt in the folder.
     2. Run MTS session
-    3. cut new files to the new MTS session.
+    3. cut new files to the "mts_measurement_7z_folder" with new name.
+    4. loop for all files.
 """
 
 mts_measurement_7z_folder = r'C:\MTS_TEST_SCRIPT\MTS_MEAS_VER' #folder with all mts_measurement as 7z format
@@ -26,27 +28,32 @@ CONFIG_PATH = r"C:\MTS_TEST_SCRIPT\extractor_vers2.cfg"
 recording_path = r"C:\MTS_TEST_SCRIPT\2022.06.07_at_20.48.26_camera-mi_804_truck_with_trailer.rrec"
 MTS_DIR = r"C:\CARTICA\MTS_21"
 
-mts_path = os.path.join(MTS_DIR, "MTS", "mts_system", "measapp.exe")
+
+# build path for MTS to reach measapp.exe
+mts_path = os.path.join(MTS_DIR, "MTS", "mts_system", "measapp.exe") #add path, not regarding OS system
 MTS_CMD = [mts_path, f"-lc{CONFIG_PATH}", f"-lr{recording_path}", "-pal", "-eab", "-silent", "-eoe"]
 
-#get 7z file name from directory:
+#get 7z files name from directory:
 onlyfiles = [f for f in listdir(mts_measurement_7z_folder) if isfile(join(mts_measurement_7z_folder, f))]
-print("fileslist" , onlyfiles)
+print("all versions list: \n" , onlyfiles)
 
-# Delete current folder and extrtact 7z files
 for onlyfile in onlyfiles:
-    # extract new files:
-    my_path_file = mts_measurement_7z_folder + '\\' + onlyfile
-    print("debug:*********** my_path_file:", my_path_file)
-    print("current:", my_path_file)
+    # extract new files from 7z folder to destination:
+    my_path_file = os.path.join(mts_measurement_7z_folder,onlyfile)
     with py7zr.SevenZipFile(my_path_file,mode='r') as z:
         z.extractall(mts_measurement_original_path )
-    #run MTS session
+        print("7z extracted : \n" , my_path_file)
+
+    # run MTS session:
+    print("MTS session starts")
     os_utilities.run_cmd(MTS_CMD)
-    #cut current folder after script finishes:
-    source_dir = mts_measurement_original_path + '\\' + 'mts_measurement'
-    destination_dir = os.path.splitext ( mts_measurement_7z_folder+ '\\' + onlyfile)[0] + 'copy'
-    print("destination_dir", destination_dir)
+    print("MTS session finish")
+
+    # cut current folder after script finishes:
+    source_dir = os.path.join(mts_measurement_original_path ,'mts_measurement')
+    destination_dir = os.path.splitext(os.path.join(mts_measurement_7z_folder,onlyfile))[0] + '_copy'
+
+    print("Success, see results at: \n", destination_dir)
     shutil.move(source_dir, destination_dir)
 
 
