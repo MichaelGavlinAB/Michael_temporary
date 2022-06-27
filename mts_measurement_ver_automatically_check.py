2af33a334c6d051ff29c1fca6fc258e6057cd8a2
MichaelGavlinAB <105149503+MichaelGavlinAB@users.noreply.github.com>
2022-06-27 16:57:07 +0100

    Add files via upload

--- mts_measurement_ver_automatically_check.py
+++ mts_measurement_ver_automatically_check.py
@@ -26,34 +26,36 @@ mts_measurement_7z_folder = r'C:\MTS_TEST_SCRIPT\MTS_MEAS_VER' #folder with all 
 mts_measurement_original_path = r'C:\MFC520_Cartex\06_Deliverables\MTS' #location of the mts_measurement folder
 CONFIG_PATH = r"C:\MTS_TEST_SCRIPT\extractor_vers2.cfg"
 recording_path = r"C:\MTS_TEST_SCRIPT\2022.06.07_at_20.48.26_camera-mi_804_truck_with_trailer.rrec"
-MTS_DIR = r"C:\CARTICA\MTS_21"
-
+measapp_path = r"C:\MFC520_Cartex\MTS\05_Testing\MTS\mts_system\measapp.exe"
 
 # build path for MTS to reach measapp.exe
-mts_path = os.path.join(MTS_DIR, "MTS", "mts_system", "measapp.exe") #add path, not regarding OS system
-MTS_CMD = [mts_path, f"-lc{CONFIG_PATH}", f"-lr{recording_path}", "-pal", "-eab", "-silent", "-eoe"]
+# mts_path = os.path.join(MTS_DIR, "MTS", "mts_system", "measapp.exe") #add path, not regarding OS system
+
+#config
+MTS_CMD = [measapp_path, f"-lc{CONFIG_PATH}", f"-lr{recording_path}", "-pal", "-eab", "-silent", "-eoe"]
 
 #get 7z files name from directory:
 onlyfiles = [f for f in listdir(mts_measurement_7z_folder) if isfile(join(mts_measurement_7z_folder, f))]
-print("all versions list: \n" , onlyfiles)
+print("All versions list: \n", onlyfiles)
 
 for onlyfile in onlyfiles:
+    print("--------------------------------------------------------------------")
     # extract new files from 7z folder to destination:
     my_path_file = os.path.join(mts_measurement_7z_folder,onlyfile)
     with py7zr.SevenZipFile(my_path_file,mode='r') as z:
         z.extractall(mts_measurement_original_path )
-        print("7z extracted : \n" , my_path_file)
+        print("--- 7z extracted : \n" , my_path_file)
 
     # run MTS session:
-    print("MTS session starts")
+    print("--- MTS session starts")
     os_utilities.run_cmd(MTS_CMD)
-    print("MTS session finish")
+    print("--- MTS session finish")
 
     # cut current folder after script finishes:
     source_dir = os.path.join(mts_measurement_original_path ,'mts_measurement')
     destination_dir = os.path.splitext(os.path.join(mts_measurement_7z_folder,onlyfile))[0] + '_copy'
 
-    print("Success, see results at: \n", destination_dir)
+    print("--- Success, see results at: \n", destination_dir)
     shutil.move(source_dir, destination_dir)
 
 
