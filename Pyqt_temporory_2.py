bb4411ee067cbfb90a5ab70f001d911dfa15d344
MichaelGavlinAB <105149503+MichaelGavlinAB@users.noreply.github.com>
2022-07-21 15:42:38 +0100

    Add files via upload

--- Pyqt_temporory_2.py
+++ Pyqt_temporory_2.py
@@ -1,9 +1,8 @@
 import PyQt5.QtWidgets as qtw
-from PyQt5.QtWidgets import QApplication, QLabel , QWidget , QPushButton , QVBoxLayout ,QMessageBox ,QFileDialog ,QComboBox,QMainWindow
+from PyQt5.QtWidgets import QApplication, QLabel , QWidget , QPushButton , QVBoxLayout ,QMessageBox ,QFileDialog,QSizePolicy ,QComboBox,QMainWindow
 import pandas as pd
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
+import pyqtgraph as pg
+
 
 
 class MainWindow(qtw.QWidget):
@@ -11,10 +10,10 @@ class MainWindow(qtw.QWidget):
         super().__init__()
         self.setWindowTitle("System engineering")
         self.setLayout(qtw.QVBoxLayout())
-        self.keypad()
-
+        self.MyUI()
         self.show()
-    def keypad(self):
+
+    def MyUI(self):
         container = qtw.QWidget()
         container.setLayout(qtw.QGridLayout())
 
@@ -22,11 +21,13 @@ class MainWindow(qtw.QWidget):
         self.btn_open_csv = qtw.QPushButton('click here to choose a csv file',clicked =self.QPushButton_clicked)
         self.Show_content = qtw.QPushButton('Show content', clicked=self.Choose_from_list)
         self.Csv_dropdown_menu = qtw.QComboBox()
+        self.graphWidget = pg.PlotWidget() #plot
 
         # adding to container
         container.layout().addWidget(self.btn_open_csv,0,0,1,2)
         container.layout().addWidget(self.Csv_dropdown_menu,1,0,1,2)
         container.layout().addWidget(self.Show_content, 1, 2, 1, 2)
+        container.layout().addWidget(self.graphWidget, 3, 1, 1, 1)
         self.layout().addWidget(container)
 
 # opens csv file search box
@@ -36,18 +37,29 @@ class MainWindow(qtw.QWidget):
         CSV_path_str = CSV_path[0][0]
         dff = pd.read_csv(CSV_path_str)
         headers = dff.columns.tolist()
-        print(headers)
+        # print(headers)
         # add headers to drop down list
         for header in headers:
-            print(header)
             self.Csv_dropdown_menu.addItem(header)
 
     def Choose_from_list(self):
-        print(self.Csv_dropdown_menu.currentText())
+        header = self.Csv_dropdown_menu.currentText()
+        path =  "/home/michael/2022.07.11_at_13.30.52_camera-mi_804_35m_l_r_Bike_cross_without_vest_bike_ped.csv"
+        # print(header)
+        hour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,0]
+        temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45,0]
+        self.get_pandas(path,header)
+        self.plot(self.graphWidget,hour,temperature)
+
+    def get_pandas(self , path,header):
+        dff = pd.read_csv(path)
+        dff.iloc[:,:1]
 
 
 
 
+    def plot(self,widget,x_data,y_daya):
+        widget.plot(x_data, y_daya) # plot the data
 
 
 
